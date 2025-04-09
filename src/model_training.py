31f10227052a6bdc6be6638c168a4daa6ab87313
Yash Sontakke <75352487+yash8484@users.noreply.github.com>
2025-04-09 15:15:32 +0530

    commit

--- src/model_training.py
+++ src/model_training.py
@@ -3,12 +3,12 @@ import pandas as pd
 import joblib
 from sklearn.model_selection import RandomizedSearchCV
 import lightgbm as lgb
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from src.logger import get_logger
 from src.custom_exception import CustomException
 from config.paths_config import *
 from config.model_params import *
-from utils.common_functions import read_yaml,load_data
+from utils.common_functions import read_yaml, load_data
 from scipy.stats import randint
 
 import mlflow
@@ -18,13 +18,18 @@ logger = get_logger(__name__)
 
 class ModelTraining:
 
-    def __init__(self,train_path,test_path,model_output_path):
+    def __init__(self, train_path, test_path, model_output_path):
         self.train_path = train_path
         self.test_path = test_path
         self.model_output_path = model_output_path
 
         self.params_dist = LIGHTGM_PARAMS
         self.random_search_params = RANDOM_SEARCH_PARAMS
+
+    @staticmethod
+    def sanitize_column_names(df):
+        df.columns = df.columns.str.replace(r'[^A-Za-z0-9_]+', '_', regex=True)
+        return df
 
     def load_and_split_data(self):
         try:
@@ -40,59 +45,59 @@ class ModelTraining:
             X_test = test_df.drop(columns=["booking_status"])
             y_test = test_df["booking_status"]
 
-            logger.info("Data splitted sucefully for Model Training")
+            # Sanitize both train and test feature names
+            X_train = self.sanitize_column_names(X_train)
+            X_test = self.sanitize_column_names(X_test)
 
-            return X_train,y_train,X_test,y_test
+            logger.info("Data split and sanitized successfully for Model Training")
+
+            return X_train, y_train, X_test, y_test
+
         except Exception as e:
             logger.error(f"Error while loading data {e}")
-            raise CustomException("Failed to load data" ,  e)
-        
-    def train_lgbm(self,X_train,y_train):
-        try:
-            logger.info("Intializing our model")
+            raise CustomException("Failed to load data", e)
 
+    def train_lgbm(self, X_train, y_train):
+        try:
+            logger.info("Initializing our model")
             lgbm_model = lgb.LGBMClassifier(random_state=self.random_search_params["random_state"])
 
-            logger.info("Starting our Hyperparamter tuning")
-
+            logger.info("Starting our Hyperparameter tuning")
             random_search = RandomizedSearchCV(
                 estimator=lgbm_model,
                 param_distributions=self.params_dist,
-                n_iter = self.random_search_params["n_iter"],
-                cv = self.random_search_params["cv"],
+                n_iter=self.random_search_params["n_iter"],
+                cv=self.random_search_params["cv"],
                 n_jobs=self.random_search_params["n_jobs"],
                 verbose=self.random_search_params["verbose"],
                 random_state=self.random_search_params["random_state"],
                 scoring=self.random_search_params["scoring"]
             )
 
-            logger.info("Starting our Hyperparamter tuning")
+            random_search.fit(X_train, y_train)
 
-            random_search.fit(X_train,y_train)
-
-            logger.info("Hyperparamter tuning completed")
-
+            logger.info("Hyperparameter tuning completed")
             best_params = random_search.best_params_
             best_lgbm_model = random_search.best_estimator_
 
-            logger.info(f"Best paramters are : {best_params}")
+            logger.info(f"Best parameters are: {best_params}")
 
             return best_lgbm_model
-        
+
         except Exception as e:
             logger.error(f"Error while training model {e}")
-            raise CustomException("Failed to train model" ,  e)
-    
-    def evaluate_model(self , model , X_test , y_test):
+            raise CustomException("Failed to train model", e)
+
+    def evaluate_model(self, model, X_test, y_test):
         try:
             logger.info("Evaluating our model")
 
             y_pred = model.predict(X_test)
 
-            accuracy = accuracy_score(y_test,y_pred)
-            precision = precision_score(y_test,y_pred)
-            recall = recall_score(y_test,y_pred)
-            f1 = f1_score(y_test,y_pred)
+            accuracy = accuracy_score(y_test, y_pred)
+            precision = precision_score(y_test, y_pred)
+            recall = recall_score(y_test, y_pred)
+            f1 = f1_score(y_test, y_pred)
 
             logger.info(f"Accuracy Score : {accuracy}")
             logger.info(f"Precision Score : {precision}")
@@ -100,41 +105,40 @@ class ModelTraining:
             logger.info(f"F1 Score : {f1}")
 
             return {
-                "accuracy" : accuracy,
-                "precison" : precision,
-                "recall" : recall,
-                "f1" : f1
+                "accuracy": accuracy,
+                "precision": precision,
+                "recall": recall,
+                "f1": f1
             }
+
         except Exception as e:
             logger.error(f"Error while evaluating model {e}")
-            raise CustomException("Failed to evaluate model" ,  e)
-        
-    def save_model(self,model):
-        try:
-            os.makedirs(os.path.dirname(self.model_output_path),exist_ok=True)
+            raise CustomException("Failed to evaluate model", e)
 
-            logger.info("saving the model")
-            joblib.dump(model , self.model_output_path)
+    def save_model(self, model):
+        try:
+            os.makedirs(os.path.dirname(self.model_output_path), exist_ok=True)
+            logger.info("Saving the model")
+            joblib.dump(model, self.model_output_path)
             logger.info(f"Model saved to {self.model_output_path}")
 
         except Exception as e:
             logger.error(f"Error while saving model {e}")
-            raise CustomException("Failed to save model" ,  e)
-    
+            raise CustomException("Failed to save model", e)
+
     def run(self):
         try:
             with mlflow.start_run():
                 logger.info("Starting our Model Training pipeline")
-
                 logger.info("Starting our MLFLOW experimentation")
 
-                logger.info("Logging the training and testing datset to MLFLOW")
-                mlflow.log_artifact(self.train_path , artifact_path="datasets")
-                mlflow.log_artifact(self.test_path , artifact_path="datasets")
+                logger.info("Logging the training and testing dataset to MLFLOW")
+                mlflow.log_artifact(self.train_path, artifact_path="datasets")
+                mlflow.log_artifact(self.test_path, artifact_path="datasets")
 
-                X_train,y_train,X_test,y_test =self.load_and_split_data()
-                best_lgbm_model = self.train_lgbm(X_train,y_train)
-                metrics = self.evaluate_model(best_lgbm_model ,X_test , y_test)
+                X_train, y_train, X_test, y_test = self.load_and_split_data()
+                best_lgbm_model = self.train_lgbm(X_train, y_train)
+                metrics = self.evaluate_model(best_lgbm_model, X_test, y_test)
                 self.save_model(best_lgbm_model)
 
                 logger.info("Logging the model into MLFLOW")
@@ -144,12 +148,13 @@ class ModelTraining:
                 mlflow.log_params(best_lgbm_model.get_params())
                 mlflow.log_metrics(metrics)
 
-                logger.info("Model Training sucesfullly completed")
+                logger.info("Model Training successfully completed")
 
         except Exception as e:
             logger.error(f"Error in model training pipeline {e}")
-            raise CustomException("Failed during model training pipeline" ,  e)
-        
-if __name__=="__main__":
-    trainer = ModelTraining(PROCESSED_TRAIN_DATA_PATH,PROCESSED_TEST_DATA_PATH,MODEL_OUTPUT_PATH)
+            raise CustomException("Failed during model training pipeline", e)
+
+
+if __name__ == "__main__":
+    trainer = ModelTraining(PROCESSED_TRAIN_DATA_PATH, PROCESSED_TEST_DATA_PATH, MODEL_OUTPUT_PATH)
     trainer.run()
